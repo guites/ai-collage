5d4b1ba749200526e83a2e417677de381d124a8f
Guilherme Garcia <gui.garcia67@gmail.com>
2024-07-28 00:17:01 -0300

    Allows user to select the best images for a preview

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ let detecting = false;
 // global HTML element
 const toggleVideoEl = document.getElementById("toggleVideoEl");
 const toggleDetectingEl = document.getElementById("toggleDetectingEl");
+const cropsWrapper = document.getElementById("snapshots");
+const collageWrapper = document.getElementById("collage-wrapper");
+const collageSnapshots = document.getElementById("collage-snapshots");
+const createCollageBtn = document.getElementById("create-collage-btn");
+const catNameEl = document.getElementById("cat-name");
+const catNameHeadingEl = document.getElementById("cat-name-heading");
+
+// initializes the masonry for the collage
+const msnry = new Masonry(collageSnapshots, {
+  itemSelector: ".cat-snapshot",
+  columnWidth: 205,
+});
 
 // set cursor to wait until video elment is loaded
 document.body.style.cursor = "wait";
@@ -22,10 +34,12 @@ function preload() {
 // The setup() function is called once when the program starts.
 function setup() {
   // create canvas element with 640 width and 480 height in pixel
-  createCanvas(640, 480);
+  const canvas = createCanvas(640, 480);
+  canvas.parent("canvasWrapper");
   // Creates a new HTML5 <video> element that contains the audio/video feed from a webcam.
   // The element is separate from the canvas and is displayed by default.
   video = createCapture(VIDEO);
+  video.parent("videoWrapper");
   video.size(640, 480);
   console.log("video element is created");
   video.elt.addEventListener("loadeddata", function () {
@@ -48,7 +62,7 @@ function draw() {
   // draw all detected objects to the canvas
   for (let i = 0; i < detections.length; i++) {
     const detection = detections[i];
-    // if (detection.label != "cell phone") continue;
+    if (detection.label != "cat") continue;
     drawResult(detection);
   }
 }
@@ -65,21 +79,98 @@ Exaple of an detect object
 }
 */
 function drawResult(object) {
-  drawDetectedObject(object);
-  drawBoundingBox(object);
-  drawLabel(object);
-}
-
-// draws the rectangle with the object's pixels
-function drawDetectedObject(object) {
   const detectedObject = video.get(
     object.x,
     object.y,
     object.width,
     object.height
   );
+  drawDetectedObject(detectedObject, object);
+  drawBoundingBox(object);
+  drawLabel(object);
+  saveCrop(detectedObject);
+}
+
+function stagedImageDiv(imgSrc) {
+  const div = document.createElement("div");
+  const img = document.createElement("img");
+  const btnDelete = document.createElement("button");
+  const deleteIcon = document.createElement("span");
+
+  div.classList.add("cat-snapshot");
+  img.src = imgSrc;
+
+  btnDelete.classList.add("action");
+  deleteIcon.classList.add("material-symbols-outlined", "delete");
+  deleteIcon.innerText = "delete";
+
+  btnDelete.addEventListener("click", (e) => {
+    e.target.closest(".cat-snapshot").remove();
+    msnry.layout();
+    const croppedHtml = croppedImageDiv(imgSrc);
+    cropsWrapper.prepend(croppedHtml);
+  });
+
+  btnDelete.appendChild(deleteIcon);
+  div.appendChild(img);
+  div.appendChild(btnDelete);
+
+  return div;
+}
+
+function croppedImageDiv(imgSrc) {
+  const div = document.createElement("div");
+  const img = document.createElement("img");
+  const btnCheck = document.createElement("button");
+  const checkIcon = document.createElement("span");
+  const btnDelete = document.createElement("button");
+  const deleteIcon = document.createElement("span");
+
+  div.classList.add("cat-snapshot");
+  img.src = imgSrc;
+  btnCheck.classList.add("action");
+  checkIcon.classList.add("material-symbols-outlined", "check");
+  checkIcon.innerText = "check";
+  btnDelete.classList.add("action");
+  deleteIcon.classList.add("material-symbols-outlined", "delete");
+  deleteIcon.innerText = "delete";
+
+  btnCheck.addEventListener("click", (e) => {
+    if (collageSnapshots.childElementCount >= 9) {
+      alert("Você só pode selecionar 9 imagens para a sua montagem!");
+      return;
+    }
+    e.target.closest(".cat-snapshot").remove();
+    const staged = stagedImageDiv(imgSrc);
+    collageSnapshots.appendChild(staged);
+    msnry.appended(staged);
+  });
+
+  btnDelete.addEventListener("click", (e) => {
+    e.target.closest(".cat-snapshot").remove();
+  });
+
+  btnCheck.appendChild(checkIcon);
+  btnDelete.appendChild(deleteIcon);
+  div.appendChild(img);
+  div.appendChild(btnCheck);
+  div.appendChild(btnDelete);
+
+  return div;
+}
+
+// saves crop as html element
+function saveCrop(detectedObject) {
+  const croppedHtml = croppedImageDiv(detectedObject.canvas.toDataURL());
+  cropsWrapper.prepend(croppedHtml);
+}
+
+// draws the rectangle with the object's pixels
+function drawDetectedObject(detectedObject, object) {
   image(detectedObject, object.x, object.y);
 }
+
+function imageWithBtns() {}
 
 // draw bounding box around the detected object
 function drawBoundingBox(object) {
@@ -127,10 +218,10 @@ function detect() {
 function toggleVideo() {
   if (!video) return;
   if (videoVisibility) {
-    video.hide();
+    video.elt.style.visibility = "hidden";
     toggleVideoEl.innerText = "Show Video";
   } else {
-    video.show();
+    video.elt.style.visibility = "visible";
     toggleVideoEl.innerText = "Hide Video";
   }
   videoVisibility = !videoVisibility;
@@ -146,3 +237,19 @@ function toggleDetecting() {
   }
   detecting = !detecting;
 }
+
+catNameEl.addEventListener("input", (e) => {
+  catNameHeadingEl.innerText = e.target.value;
+});
+
+createCollageBtn.addEventListener("click", () => {
+  const catName = catNameEl.value;
+  if (!catName || catName === "") {
+    alert("Digite o nome do seu gato!");
+    return;
+  }
+  if (collageSnapshots.childElementCount < 1) {
+    alert("Selecione ao menos uma imagem capturada pela webcam!");
+    return;
+  }
+});
